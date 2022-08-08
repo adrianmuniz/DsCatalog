69788aa811a7587aa08534c269d884cd06f6d154
Cainã Adrian <adrianmuniz.ceo@outlook.com>
2022-08-08 10:29:48 -0300

    DTO

--- backend/src/main/java/com/devsuperior/dscatalog/Resources/CategoryResources.java
+++ backend/src/main/java/com/devsuperior/dscatalog/Resources/CategoryResources.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.service.CategoryService;
 
@@ -20,8 +21,8 @@ public class CategoryResources {
 	
 	//encapsular o http
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll() {
-		List<Category> list = service.findAll();
+	public ResponseEntity<List<CategoryDTO>> findAll() {
+		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
