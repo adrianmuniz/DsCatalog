bc91ba341aa435c78e371fba487d4bbe4d287152
Cainã Adrian <adrianmuniz.ceo@outlook.com>
2022-09-07 13:09:41 -0300

    deleteShouldThrowResourceNotFoundExceptionWhenIdDoesExist

--- backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
@@ -1,5 +1,9 @@
 package com.devsuperior.dscatalog.services;
 
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,6 +16,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.service.ProductService;
+import com.devsuperior.dscatalog.service.exceptions.ResourceNotFoundException;
 
 @ExtendWith(SpringExtension.class)
 public class ProductServiceTests {
@@ -32,10 +37,10 @@ public class ProductServiceTests {
 		nonExistingId = 1000L;
 		
 		//Simulando o comportamento que o deleteById deve fazer quando existe Id
-		Mockito.doNothing().when(repository).deleteById(existingId);
+		doNothing().when(repository).deleteById(existingId);
 		
 		//Simulando o comportamento que o deleteById deve fazer quando não existe Id
-		Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
+		doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
 	}
 	
 	@Test
@@ -45,6 +50,16 @@ public class ProductServiceTests {
 			service.delete(existingId);
 		});
 		
-		Mockito.verify(repository, Mockito.times(1)).deleteById(existingId);
+		verify(repository, Mockito.times(1)).deleteById(existingId);
+	}
+	
+	@Test
+	public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesExist() {
+		
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			service.delete(nonExistingId);
+		});
+		
+		verify(repository, Mockito.times(1)).deleteById(nonExistingId);
 	}
 }
