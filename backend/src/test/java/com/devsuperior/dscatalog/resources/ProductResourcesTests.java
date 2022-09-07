6433ddc267f11eea7bd4f49ab337c7e347eb4866
Cainã Adrian <adrianmuniz.ceo@outlook.com>
2022-09-07 19:52:57 -0300

    ProductResourcesTests

---
+++ backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourcesTests.java
@@ -0,0 +1,10 @@
+package com.devsuperior.dscatalog.resources;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import com.devsuperior.dscatalog.Resources.ProductResources;
+
+@WebMvcTest(ProductResources.class)
+public class ProductResourcesTests {
+
+}
