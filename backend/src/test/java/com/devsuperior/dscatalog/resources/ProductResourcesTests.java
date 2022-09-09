d73e180181ba22ceb76e69a4e94f619508c3c06e
Cainã Adrian <adrianmuniz.ceo@outlook.com>
2022-09-08 21:55:41 -0300

    Legibilidade e negociação de Conteudo

--- backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourcesTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourcesTests.java
@@ -1,16 +1,20 @@
 package com.devsuperior.dscatalog.resources;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import com.devsuperior.dscatalog.Resources.ProductResources;
 import com.devsuperior.dscatalog.dto.ProductDTO;
@@ -37,5 +41,15 @@ public class ProductResourcesTests {
 		page = new PageImpl<>(List.of(productDTO));
 		
 		when(service.findAllPaged(ArgumentMatchers.any())).thenReturn(page);
+	}
+	
+	@Test
+	public void findAllShouldReturnPage() throws Exception {
+		
+		ResultActions result = mockMvc.perform(get("/products")
+					.accept()
+				);
+		
+		result.andExpect(status().isOk());
 	}
 }
