80c8c7260774e2961b31f5213aa7e5f61aded4e0
Cainã Adrian <adrianmuniz.ceo@outlook.com>
2022-09-10 13:32:34 -0300

    deleteShouldDeleteResourceWhenIdExist

--- backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceIT.java
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceIT.java
@@ -1,9 +1,12 @@
 package com.devsuperior.dscatalog.services;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.service.ProductService;
 
 @SpringBootTest
@@ -11,6 +14,9 @@ public class ProductServiceIT {
 
 	@Autowired
 	private ProductService service;
+	
+	@Autowired
+	private ProductRepository repository;
 	
 	private long existingId;
 	private long nonExistingId;
@@ -21,5 +27,13 @@ public class ProductServiceIT {
 		existingId = 1L;
 		nonExistingId = 1000L;
 		countTotalProducts = 25L;
+	}
+	
+	@Test
+	public void deleteShouldDeleteResourceWhenIdExist() {
+		
+		service.delete(existingId);
+		
+		Assertions.assertEquals(countTotalProducts - 1, repository.count());
 	}
 }
