02c63c0e85412bd93efe9c2884d532692068a8bf
Cainã Adrian <adrianmuniz.ceo@outlook.com>
2022-09-10 13:29:10 -0300

    Criando Teste de Integração

---
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceIT.java
@@ -0,0 +1,25 @@
+package com.devsuperior.dscatalog.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.devsuperior.dscatalog.service.ProductService;
+
+@SpringBootTest
+public class ProductServiceIT {
+
+	@Autowired
+	private ProductService service;
+	
+	private long existingId;
+	private long nonExistingId;
+	private long countTotalProducts;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		existingId = 1L;
+		nonExistingId = 1000L;
+		countTotalProducts = 25L;
+	}
+}
