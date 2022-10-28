7c2f092964fc05d7d6de87a866fd3fa063547bb9
Cainã Adrian Muniz <cadrian@minsait.com>
2022-10-27 21:15:45 -0300

    UserUpdateValidator

--- backend/src/main/java/com/devsuperior/dscatalog/service/validation/UserUpdateValidator.java
+++ backend/src/main/java/com/devsuperior/dscatalog/service/validation/UserUpdateValidator.java
@@ -2,39 +2,45 @@ package com.devsuperior.dscatalog.service.validation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
 
 import com.devsuperior.dscatalog.Resources.exceptions.FieldMessage;
-import com.devsuperior.dscatalog.dto.UserInsertDTO;
+import com.devsuperior.dscatalog.dto.UserUpdateDTO;
 import com.devsuperior.dscatalog.entities.User;
 import com.devsuperior.dscatalog.repositories.UserRepository;
 
-public class UserInsertValidator implements ConstraintValidator<UserInsertValid, UserInsertDTO> {
+public class UserUpdateValidator implements ConstraintValidator<UserUpdateValid, UserUpdateDTO> {
+	
+	@Autowired
+	private HttpServletRequest request;
 	
 	@Autowired
 	private UserRepository userRepository;
 	
 	@Override
-	public void initialize(UserInsertValid ann) {
+	public void initialize(UserUpdateValid ann) {
 	}
 
 	@Override
-	public boolean isValid(UserInsertDTO dto, ConstraintValidatorContext context) {
+	public boolean isValid(UserUpdateDTO dto, ConstraintValidatorContext context) {
+		
+		@SuppressWarnings("unchecked")
+		var uriVars = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		long userId = Long.parseLong(uriVars.get("id"));
 		
 		List<FieldMessage> list = new ArrayList<>();
 		
-		// Coloque aqui seus testes de validação, acrescentando objetos FieldMessage à lista
-		
 		User user = userRepository.findByEmail(dto.getEmail());
-		if(user != null) {
+		if(user != null && userId != user.getId()) {
 			list.add(new FieldMessage("email", "Email já existe"));
 		}
-		
-		
 		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
